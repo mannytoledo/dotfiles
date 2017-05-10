be4b4eebf58d7fd6973ce775e390df82e48818b7
Manny Toledo <mannytoledo@users.noreply.github.com>
2017-05-10 18:11:00 -0400

    Add tunnel aliases, fix vim pad, ansible file syntax fix (#8)

--- zsh/zsh.symlink/helpers/aliases.zsh
+++ zsh/zsh.symlink/helpers/aliases.zsh
@@ -2,3 +2,6 @@ alias git="hub"
 alias py3init="echo 'layout python3' > .envrc && direnv allow"
 alias py2init="echo 'layout python' > .envrc && direnv allow"
 alias dc="docker-compose"
+alias ptunnel='ssh -N behancecompute-prod-jump-tunnel &'
+alias stunnel='ssh -N behancecompute-stage-jump-tunnel &'
+alias dtunnel='ssh -N behancecompute-dev-jump-tunnel &'
