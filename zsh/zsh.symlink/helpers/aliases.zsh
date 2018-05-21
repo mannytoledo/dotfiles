80d2ea81803f9ba1492dac1923bb4bfdf7f31158
Manny Toledo <mtoledo@adobe.com>
2018-05-21 08:45:41 -0400

    Update aliases and add kitty config

--- zsh/zsh.symlink/helpers/aliases.zsh
+++ zsh/zsh.symlink/helpers/aliases.zsh
@@ -1,7 +1,9 @@
 alias git="hub"
 alias py3init="echo 'layout python3' > .envrc && direnv allow"
 alias py2init="echo 'layout python' > .envrc && direnv allow"
+alias corpify="touch .envrc && echo 'export GITHUB_HOST=git.corp.adobe.com' >> .envrc && direnv allow"
 alias dc="docker-compose"
-alias ptunnel='ssh -N behancecompute-prod-jump-tunnel &'
-alias stunnel='ssh -N behancecompute-stage-jump-tunnel &'
-alias dtunnel='ssh -N behancecompute-dev-jump-tunnel &'
+alias dm="docker-machine"
+alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
+alias chefenv="eval $(chef shell-init zsh)"
+alias kittyconfig="nvim ~/.config/kitty/kitty.conf"
