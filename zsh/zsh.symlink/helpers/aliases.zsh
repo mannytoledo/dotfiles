ff36ddb4eee6fbaa2a6fc3c6ae675dc7583bb1bc
Manny Toledo <mtoledo@adobe.com>
2019-07-27 11:10:24 -0400

    COnfig fixes and zsh tweaks

--- zsh/zsh.symlink/helpers/aliases.zsh
+++ zsh/zsh.symlink/helpers/aliases.zsh
@@ -1,7 +1,9 @@
 alias git="hub"
-alias py3init="echo 'use python 3.7.0' > .envrc && direnv allow"
+alias py3init="echo 'use python 3.7.1' > .envrc && direnv allow"
+alias pynviminit="pip install python-language-server pyls-isort pyls-black pyls-mypy"
 alias py36init="echo 'use python 3.6.5' > .envrc && direnv allow"
 alias py2init="echo 'use python 2.7.15' > .envrc && direnv allow"
+alias pyenvbrewinstall='CFLAGS="-I$(brew --prefix openssl)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib" pyenv install'
 alias corpify="touch .envrc && echo 'export GITHUB_HOST=git.corp.adobe.com' >> .envrc && direnv allow"
 alias dc="docker-compose"
 alias dm="docker-machine"
